59d58f3acf8c703bc12c9a4b1d6796cab6e3ac10
Aftaab Siddiqui <aftaab2507@gmail.com>
2024-12-13 02:21:54 +0530

    Add PageResponse Payload

---
+++ src/main/java/com/maskedsyntax/blog/payload/PostResponse.java
@@ -0,0 +1,17 @@
+package com.maskedsyntax.blog.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class PostResponse {
+    private List<PostDTO> content;
+    private int pageNo;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+    private boolean lastPage;
+}
