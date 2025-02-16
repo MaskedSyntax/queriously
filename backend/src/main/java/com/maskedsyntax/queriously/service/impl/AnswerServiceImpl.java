6e1725637fbdb0e5de31e542db0938ef95592a52
Aftaab Siddiqui <aftaab2507@gmail.com>
2025-02-16 16:54:56 +0530

    remove unnecessary imports

--- backend/src/main/java/com/maskedsyntax/queriously/service/impl/AnswerServiceImpl.java
+++ backend/src/main/java/com/maskedsyntax/queriously/service/impl/AnswerServiceImpl.java
@@ -1,7 +1,6 @@
 package com.maskedsyntax.queriously.service.impl;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
